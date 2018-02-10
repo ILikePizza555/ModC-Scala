76908db1b5cf6cf3f0b62414c91accb62f9622c1
Isaac Avram <avrisaac555@gmail.com>
2018-02-10 14:02:32 -0800

    Added a TestSpec class for ScalaTest

--- src/tests/scala/com/avrisaac555/modc/tests/TokenizerSpec.scala
+++ src/tests/scala/com/avrisaac555/modc/tests/TokenizerSpec.scala
@@ -1,9 +1,29 @@
 package com.avrisaac555.modc.tests
 
 import org.scalatest._
+import com.avrisaac555.modc._
+import org.scalatest.prop.TableDrivenPropertyChecks
 
-class TokenizerSpec extends FlatSpec with Matchers {
-    "tokenize" should "return a single Token" in {
+import scala.collection.immutable.ListMap
 
+class TokenizerSpec extends FlatSpec with Matchers with TableDrivenPropertyChecks {
+    val TokenTable = Table(
+        ("Token", "Type"),
+        ("{", OPEN_BRACE),
+        ("}", CLOSE_BRACE),
+        ("(", OPEN_PAREN),
+        (")", CLOSE_PAREN),
+        (";", LINE_END),
+        ("module", KEY_MODULE),
+        ("int", KEY_INT),
+        ("return", KEY_RETURN),
+        ("identifier", IDENTIFIER),
+        ("555666777", LIT_INT)
+    )
+
+    forAll (TokenTable) { (token: String, expected: Option[String] => Token) =>
+        "tokenize" should "return a single Token" in {
+
+        }
     }
 }

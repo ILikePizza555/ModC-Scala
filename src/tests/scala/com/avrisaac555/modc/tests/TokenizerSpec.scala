c3197369440ca7095beeabe4e82644c029088ecc
Isaac Avram <avrisaac555@gmail.com>
2018-02-12 20:14:17 -0800

    Added tests for Tokenizer

--- src/tests/scala/com/avrisaac555/modc/tests/TokenizerSpec.scala
+++ src/tests/scala/com/avrisaac555/modc/tests/TokenizerSpec.scala
@@ -9,21 +9,24 @@ import scala.collection.immutable.ListMap
 class TokenizerSpec extends FlatSpec with Matchers with TableDrivenPropertyChecks {
     val TokenTable = Table(
         ("Token", "Type"),
-        ("{", OPEN_BRACE),
-        ("}", CLOSE_BRACE),
-        ("(", OPEN_PAREN),
-        (")", CLOSE_PAREN),
-        (";", LINE_END),
-        ("module", KEY_MODULE),
-        ("int", KEY_INT),
-        ("return", KEY_RETURN),
-        ("identifier", IDENTIFIER),
-        ("555666777", LIT_INT)
+        ("{", OPEN_BRACE()),
+        ("}", CLOSE_BRACE()),
+        ("(", OPEN_PAREN()),
+        (")", CLOSE_PAREN()),
+        (";", LINE_END()),
+        ("module", KEY_MODULE()),
+        ("int", KEY_INT()),
+        ("return", KEY_RETURN()),
+        ("identifier", IDENTIFIER(Some("identifier"))),
+        ("555666777", LIT_INT(Some("555666777")))
     )
 
-    forAll (TokenTable) { (token: String, expected: Option[String] => Token) =>
-        "tokenize" should "return a single Token" in {
+    "tokenizer" should "return valid tokens" in {
+        forAll(TokenTable) { (token: String, expected: Token) =>
+            val actual: List[Token] = Token.tokenize(token)
 
+            actual should have length 1
+            actual should contain(expected)
         }
     }
 }

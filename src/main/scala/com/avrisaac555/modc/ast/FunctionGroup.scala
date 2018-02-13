a36e57f3b4ca95bd511ae129e08e3a8585638e03
Isaac Avram <avrisaac555@gmail.com>
2018-02-12 21:15:25 -0800

    Added FunctionGroup

---
+++ src/main/scala/com/avrisaac555/modc/ast/FunctionGroup.scala
@@ -0,0 +1,29 @@
+package com.avrisaac555.modc.ast
+
+import com.avrisaac555.modc._
+
+/**
+  * A FunctionGroup is a single AST node that contains Tokens as children.
+  * @param name The name of the function
+  * @param tokens
+  */
+sealed class FunctionGroup(name: String, tokens: List[Token])
+
+object FunctionGroup {
+    def fromTokens(list: List[Token]) = list match {
+        case KEY_INT(_) :: IDENTIFIER(Some(s)) :: OPEN_PAREN(_) :: CLOSE_PAREN(_) :: OPEN_BRACE(_) :: xs =>
+            def switch(l: List[Token]): (List[Token], List[Token]) = {
+                val (a, b) = l span (t => !t.isInstanceOf[OPEN_PAREN] & !t.isInstanceOf[CLOSE_PAREN])
+
+                b match {
+                    case CLOSE_PAREN(_)::bs => (a, bs)
+                    case OPEN_PAREN(_)::bs =>
+                        val (c, d) = switch(bs)
+                        (a ++ c, d)
+                }
+            }
+
+            val (group, ex) = switch(xs)
+            (FunctionGroup(s, group), ex)
+    }
+}

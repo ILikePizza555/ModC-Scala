f44cec2a517d7a33e66d2574ad640882796babe1
Isaac Avram <avrisaac555@gmail.com>
2018-02-16 10:14:30 -0800

    Revert "FunctionGroup now passed tests" Revert "Added tests for FunctionGroup and Utils" Revert "Abstracted the switch inner function to switchTake" Revert "Updated FunctionGroup to actually return something"

--- src/main/scala/com/avrisaac555/modc/ast/FunctionGroup.scala
+++ src/main/scala/com/avrisaac555/modc/ast/FunctionGroup.scala
@@ -2,42 +2,28 @@ package com.avrisaac555.modc.ast
 
 import com.avrisaac555.modc._
 
-import scala.collection.mutable
-import scala.collection.mutable.ArrayBuffer
-
 /**
   * A FunctionGroup is a single AST node that contains Tokens as children.
   * @param name The name of the function
   * @param tokens
   */
-sealed class FunctionGroup(val name: String, val tokens: List[Token])
+sealed class FunctionGroup(name: String, tokens: List[Token])
 
 object FunctionGroup {
-    def loop(l: List[Token]): (List[Token], List[Token]) = {
-        val stack = mutable.Stack[Token]()
-        val rv = ArrayBuffer[Token]()
-
-        for (i <- l.indices) {
-            val n = l(i)
-
-            n match {
-                case _: OPEN_BRACE =>
-                    rv.append(n)
-                    stack.push(n)
-                case _: CLOSE_BRACE if stack.isEmpty => return (rv.toList, l.drop(i + 1))
-                case _: CLOSE_BRACE if stack.nonEmpty =>
-                    rv.append(n)
-                    stack.pop()
-                case _ => rv.append(n)
-            }
-        }
-
-        (rv.toList, Nil)
-    }
-
-    def fromTokens(list: List[Token]): (FunctionGroup, List[Token]) = list match {
+    def fromTokens(list: List[Token]) = list match {
         case KEY_INT(_) :: IDENTIFIER(Some(s)) :: OPEN_PAREN(_) :: CLOSE_PAREN(_) :: OPEN_BRACE(_) :: xs =>
-            val (tokens, excess) = loop(xs)
-            (new FunctionGroup(s, tokens), excess)
+            def switch(l: List[Token]): (List[Token], List[Token]) = {
+                val (a, b) = l span (t => !t.isInstanceOf[OPEN_PAREN] & !t.isInstanceOf[CLOSE_PAREN])
+
+                b match {
+                    case CLOSE_PAREN(_)::bs => (a, bs)
+                    case OPEN_PAREN(_)::bs =>
+                        val (c, d) = switch(bs)
+                        (a ++ c, d)
+                }
+            }
+
+            val (group, ex) = switch(xs)
+            (FunctionGroup(s, group), ex)
     }
 }
